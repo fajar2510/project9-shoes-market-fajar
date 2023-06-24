f3ced83e88e5e50815d21693433228f6521ebdb1
Fajar Abdurrohman <48176273+fajar2510@users.noreply.github.com>
2023-06-24 08:29:47 +0700

    patch-error1

--- frontend/components/ProductCard.jsx
+++ frontend/components/ProductCard.jsx
@@ -17,12 +17,12 @@ const ProductCard = ({ data: { attributes: p, id } }) => {
             <div className='p-4 text-black/[0.9]'>
                 <h2 className='text-lg font-medium'>{p.name}</h2>
                 <div className='flex items-center text-black/[0.5]'>
-                    <p className='mr-2 text-lg font-semibold'>${p.price}</p>
+                    <p className='mr-2 text-lg font-semibold'>Rp. {p.price}</p>
 
                     {p.original_price && (
                         <>
                             <p className='text-base  font-medium line-through'>
-                                ${p.original_price}
+                                Rp. {p.original_price}
                             </p>
                             <p className='ml-auto text-base font-medium text-green-500'>
                                 {getDiscountedPricePercentage(
